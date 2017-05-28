aeee8d575a52fd122f44107c2ea4ec32c1faeb60
Lee Benson <lee@leebenson.com>
2017-05-28 18:58:11 +0100

    2.4.1
    2.4.1 - 2017-05-28
    ------------------------------------------------------------
    * Bumps to kit v1.3.1

--- cli/index.js
+++ cli/index.js
@@ -33,8 +33,8 @@ const pkg = require('../package.json');
 
 const versions = {
   'kit': {
-    version: '1.3.0',
-    date: '2017-05-27',
+    version: '1.3.1',
+    date: '2017-05-28',
   },
   'kit.ts': {
     version: '1.1.2',
