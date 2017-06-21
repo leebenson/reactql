80b4bdc69a1e8e07c212c282e0fe333aafa78ccd
Lee Benson <lee@leebenson.com>
2017-06-21 16:54:28 +0100

    3.2.1
    3.2.1 - 2017-06-21
    ------------------------------------------------------------
    * Bumps to kit v1.10.1

--- cli/index.js
+++ cli/index.js
@@ -32,8 +32,8 @@ const pkg = require('../package.json');
 
 const versions = {
   'kit': {
-    version: '1.10.0',
-    date: '2017-06-20',
+    version: '1.10.1',
+    date: '2017-06-21',
   },
   /*
     OMITTING TYPESCRIPT KIT FOR NOW, per https://github.com/reactql/kit.ts/issues/18
