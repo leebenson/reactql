a9bfbd8d8099b2a64afde7c41e8e123fad56fadf
Lee Benson <lee@leebenson.com>
2017-08-26 09:04:30 +0100

    4.5.3
    4.5.3 - 2017-08-26
    ------------------------------------------------------------
    * Bumps to kit v2.5.2

--- cli/index.js
+++ cli/index.js
@@ -35,8 +35,8 @@ const pkg = require('../package.json');
 // ----------------------
 
 const kit = {
-  version: '2.5.1',
-  date: '2017-08-22',
+  version: '2.5.2',
+  date: '2017-08-26',
 }
 
 // Notice placeholder, for displaying a message back to the user before
