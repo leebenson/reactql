e25c3d6956c1230b5bd6ccee0feb746a6ae57d45
Lee Benson <lee@leebenson.com>
2017-09-07 14:30:39 +0100

    4.8.2
    4.8.2 - 2017-09-07
    ------------------------------------------------------------
    * Bumps to kit v2.8.2

--- cli/index.js
+++ cli/index.js
@@ -35,8 +35,8 @@ const pkg = require('../package.json');
 // ----------------------
 
 const kit = {
-  version: '2.8.1',
-  date: '2017-09-06',
+  version: '2.8.2',
+  date: '2017-09-07',
 }
 
 // Notice placeholder, for displaying a message back to the user before
