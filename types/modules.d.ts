e525c99f2f37a35c53872fe5d389b88de95eabcb
Dmitry Petrov <dvpetrov.main@gmail.com>
2019-01-27 02:46:33 +0300

    Add prettier && Format code

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -2,11 +2,11 @@ declare module "brotli-webpack-plugin" {
   import * as webpack from "webpack";
 
   interface IBrotliWebpackOptions {
-      minRatio: number;
+    minRatio: number;
   }
 
   class BrotliWebpackPlugin extends webpack.Plugin {
-      public constructor(opt: IBrotliWebpackOptions);
+    public constructor(opt: IBrotliWebpackOptions);
   }
 
   export = BrotliWebpackPlugin;
